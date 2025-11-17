d5cf149dcc5ee33d759ce1f5186ff37bfe3e9146
olorin99 <christianstrodl1@gmail.com>
2025-11-17 21:41:10 +1000

    Add imglink image store.

--- lib/src/api/images.dart
+++ lib/src/api/images.dart
@@ -7,7 +7,7 @@ import 'package:path/path.dart';
 import 'package:http_parser/http_parser.dart';
 import 'package:image_picker/image_picker.dart';
 
-enum ImageStore { platform, catbox }
+enum ImageStore { platform, catbox, imgLink }
 
 class APIImages {
   final ServerClient client;
@@ -24,72 +24,93 @@ class APIImages {
       store = ImageStore.catbox;
     }
 
-    switch (store) {
-      case ImageStore.platform:
-        switch (client.software) {
-          case ServerSoftware.mbin:
-            throw UnimplementedError(
-              'Direct image upload not available for Mbin',
-            );
-          case ServerSoftware.lemmy:
-            const path = '/pictrs/image';
+    try {
+      switch (store) {
+        case ImageStore.platform:
+          switch (client.software) {
+            case ServerSoftware.mbin:
+              throw UnimplementedError(
+                'Direct image upload not available for Mbin',
+              );
+            case ServerSoftware.lemmy:
+              const path = '/pictrs/image';
 
-            final request = http.MultipartRequest(
-              'POST',
-              Uri.https(client.domain, path),
-            );
-            final file = http.MultipartFile.fromBytes(
-              'images[]',
-              await image.readAsBytes(),
-              filename: basename(image.path),
-              contentType: MediaType.parse(lookupMimeType(image.path)!),
-            );
-            request.files.add(file);
+              final request = http.MultipartRequest(
+                'POST',
+                Uri.https(client.domain, path),
+              );
+              final file = http.MultipartFile.fromBytes(
+                'images[]',
+                await image.readAsBytes(),
+                filename: basename(image.path),
+                contentType: MediaType.parse(lookupMimeType(image.path)!),
+              );
+              request.files.add(file);
 
-            final response = await client.sendRequest(request);
+              final response = await client.sendRequest(request);
 
-            final imageName =
-                ((response.bodyJson['files'] as List<dynamic>).first
-                        as JsonMap)['file']
-                    as String?;
+              final imageName =
+              ((response.bodyJson['files'] as List<dynamic>).first
+              as JsonMap)['file']
+              as String?;
 
-            return 'https://${client.domain}/pictrs/image/$imageName';
+              return 'https://${client.domain}/pictrs/image/$imageName';
 
-          case ServerSoftware.piefed:
-            const path = '/upload/image';
+            case ServerSoftware.piefed:
+              const path = '/upload/image';
 
-            final request = http.MultipartRequest(
-              'POST',
-              Uri.https(client.domain, client.software.apiPathPrefix + path),
-            );
-            final file = http.MultipartFile.fromBytes(
-              'file',
-              await image.readAsBytes(),
-              filename: basename(image.path),
-              contentType: MediaType.parse(lookupMimeType(image.path)!),
-            );
-            request.files.add(file);
+              final request = http.MultipartRequest(
+                'POST',
+                Uri.https(client.domain, client.software.apiPathPrefix + path),
+              );
+              final file = http.MultipartFile.fromBytes(
+                'file',
+                await image.readAsBytes(),
+                filename: basename(image.path),
+                contentType: MediaType.parse(lookupMimeType(image.path)!),
+              );
+              request.files.add(file);
 
-            final response = await client.sendRequest(request);
+              final response = await client.sendRequest(request);
 
-            return response.bodyJson['url'] as String;
-        }
-      case ImageStore.catbox:
-        const path = 'https://catbox.moe/user/api.php';
+              return response.bodyJson['url'] as String;
+          }
+        case ImageStore.catbox:
+          const path = 'https://catbox.moe/user/api.php';
 
-        final request = http.MultipartRequest('POST', Uri.parse(path));
-        final file = http.MultipartFile.fromBytes(
-          'fileToUpload',
-          await image.readAsBytes(),
-          filename: basename(image.path),
-          contentType: MediaType.parse(lookupMimeType(image.path)!),
-        );
+          final request = http.MultipartRequest('POST', Uri.parse(path));
+          final file = http.MultipartFile.fromBytes(
+            'fileToUpload',
+            await image.readAsBytes(),
+            filename: basename(image.path),
+            contentType: MediaType.parse(lookupMimeType(image.path)!),
+          );
 
-        request.fields['reqtype'] = 'fileupload';
-        request.files.add(file);
+          request.fields['reqtype'] = 'fileupload';
+          request.files.add(file);
 
-        final response = await client.sendRequest(request);
-        return response.body;
-    } //TODO: add more image store options
+          final response = await client.sendRequest(request);
+          return response.body;
+        case ImageStore.imgLink:
+          const path = 'https://imglink.io/upload';
+
+          final request = http.MultipartRequest('POST', Uri.parse(path));
+          final file = http.MultipartFile.fromBytes(
+            'file',
+            await image.readAsBytes(),
+            filename: basename(image.path),
+            contentType: MediaType.parse(lookupMimeType(image.path)!),
+          );
+
+          request.files.add(file);
+
+          final response = await client.sendRequest(request);
+
+          return ((response.bodyJson['images'] as List<dynamic>)
+              .first as JsonMap)['direct_link'] as String;
+      } //TODO: add more image store options
+    } catch (e) {
+      return '';
+    }
   }
 }
