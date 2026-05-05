c2c5b8cbb38f9045adbd17b484952c9283524fbd
olorin99 <christianstrodl1@gmail.com>
2026-05-05 10:58:00 +1000

    Show snackbar to notify on successful image download.

--- lib/src/utils/share.dart
+++ lib/src/utils/share.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:interstellar/src/utils/globals.dart';
 import 'package:interstellar/src/utils/platform/platform.dart';
 import 'package:interstellar/src/utils/utils.dart';
 import 'package:path/path.dart';
@@ -30,9 +32,17 @@ Future<ShareResult> shareFile(Uri uri, String filename) async {
 }
 
 Future<void> downloadFile(
+  BuildContext context,
   Uri uri,
   String filename, {
   Directory? defaultDir,
 }) async {
-  downloadFromUri(uri, filename, defaultDir: defaultDir);
+  await downloadFromUri(uri, filename, defaultDir: defaultDir);
+  if (!context.mounted) return;
+  scaffoldMessengerKey.currentState?.showSnackBar(
+    SnackBar(
+      content: Text(l(context).downloaded_file(filename)),
+      showCloseIcon: true,
+    ),
+  );
 }
