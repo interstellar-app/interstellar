06cdf1dc8cbfd011db89136f03f94bc508f485a9
jwr1 <47087725+jwr1@users.noreply.github.com>
2026-05-31 19:57:49 -0400

    Remove dead instances from recommended servers list

--- lib/src/screens/settings/login_select.dart
+++ lib/src/screens/settings/login_select.dart
@@ -45,22 +45,16 @@ final Map<String, List<(String, ServerSoftware)>> _recommendedInstances = {
   'it': [
     ('feddit.it', ServerSoftware.lemmy),
     ('diggita.com', ServerSoftware.lemmy),
-    ('lemminielettrici.it', ServerSoftware.lemmy),
   ],
-  'ja': [
-    ('lm.korako.me', ServerSoftware.lemmy),
-    ('pf.korako.me', ServerSoftware.piefed),
-  ],
+  'ja': [('pf.korako.me', ServerSoftware.piefed)],
   'ko': [('lemmy.funami.tech', ServerSoftware.lemmy)],
   'lt': [('group.lt', ServerSoftware.lemmy)],
-  'ms': [('monyet.cc', ServerSoftware.lemmy)],
   'nl': [
     ('feddit.nl', ServerSoftware.lemmy),
     ('lemy.nl', ServerSoftware.lemmy),
   ],
   'pl': [
     ('szmer.info', ServerSoftware.lemmy),
-    ('fedit.pl', ServerSoftware.lemmy),
   ],
   'pt': [
     ('lemmy.eco.br', ServerSoftware.lemmy),
