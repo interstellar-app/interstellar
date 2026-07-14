cc25152e0f966fd2ce9473158aa0abd7dd997905
olorin99 <christianstrodl1@gmail.com>
2026-07-14 17:27:41 +1000

    Add top active mastodon instances to list of deep links.

--- scripts/fetch_instances.sh
+++ scripts/fetch_instances.sh
@@ -7,9 +7,9 @@ curl -H 'Content-Type: application/json' -X POST -d '{"query": "query {nodes(sof
 curl -H 'Content-Type: application/json' -X POST -d '{"query": "query {nodes(softwarename: \"mbin\") {domain}}"}' https://api.fediverse.observer 2> /dev/null | jq -r '.data.nodes | .[] | .domain' | sort | uniq -i > mbin_instances.txt
 
 # Theres a lot more mastodon instances so only include those with certain active user count.
-#curl -H 'Content-Type: application/json' -X POST -d '{"query": "query {nodes(softwarename: \"mastodon\") {domain active_users_monthly}}"}' https://api.fediverse.observer 2> /dev/null | jq -r '.data.nodes | .[] | select(.active_users_monthly > 100) | .domain' | sort | uniq -i > mastodon_instances.txt
+curl -H 'Content-Type: application/json' -X POST -d '{"query": "query {nodes(softwarename: \"mastodon\") {domain active_users_monthly}}"}' https://api.fediverse.observer 2> /dev/null | jq -r '.data.nodes | .[] | select(.active_users_monthly > 100) | .domain' | sort | uniq -i > mastodon_instances.txt
 
-cat lemmy_instances.txt piefed_instances.txt mbin_instances.txt | sort | uniq -i > instances.txt
+cat lemmy_instances.txt piefed_instances.txt mbin_instances.txt mastodon_instances.txt | sort | uniq -i > instances.txt
 
 cat << EOF > lib/src/utils/instances.dart
 import 'package:interstellar/src/controller/server.dart';
@@ -19,6 +19,8 @@ $(awk '{ print "  \047"$0"\047: ServerSoftware.mbin," }' mbin_instances.txt)
 $(awk '{ print "  \047"$0"\047: ServerSoftware.piefed," }' piefed_instances.txt)
 
 $(awk '{ print "  \047"$0"\047: ServerSoftware.lemmy," }' lemmy_instances.txt)
+
+$(awk '{ print "  \047"$0"\047: ServerSoftware.mbin," }' mastodon_instances.txt)
 };
 EOF
 
@@ -45,4 +47,4 @@ while IFS= read -r line; do
 done < android/app/src/main/AndroidManifest.xml
 mv android/app/src/main/AndroidManifest-new.xml android/app/src/main/AndroidManifest.xml
 
-rm instances.txt lemmy_instances.txt piefed_instances.txt mbin_instances.txt
+rm instances.txt lemmy_instances.txt piefed_instances.txt mbin_instances.txt mastodon_instances.txt
